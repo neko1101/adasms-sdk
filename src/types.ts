3474f80bcb9a8aff8f8abf3203d765f122ce4e37
Hafiz Nor <fizdotcom@gmail.com>
2022-11-10 23:34:51 +0800

    chore: linting
    - update tsconfig
    - bump v0.1.6
    - update format script

--- src/types.ts
+++ src/types.ts
@@ -1,133 +1,133 @@
 export interface SendSMSObject {
-    phone?: string
-    message?: string
-    callbackUrl?: string
-    previewMode?: boolean
-    lead_id?: number
-    send_at?: string
+    phone?: string;
+    message?: string;
+    callbackUrl?: string;
+    previewMode?: boolean;
+    lead_id?: number;
+    send_at?: string;
 }
 
 export interface ClientOption {
-    token: string
+    token: string;
 }
 
 export interface ErrorResponse {
-    error?: string
-    explain?: string
+    error?: string;
+    explain?: string;
 }
 
 export interface SendSMSResponse extends ErrorResponse {
-    success?: boolean
-    message?: SendSMSAPIMessage
+    success?: boolean;
+    message?: SendSMSAPIMessage;
 }
 
-export interface SendSMSAPIResponse extends ErrorResponse{
-    success?: boolean
-    message?: SendSMSAPIMessage      
+export interface SendSMSAPIResponse extends ErrorResponse {
+    success?: boolean;
+    message?: SendSMSAPIMessage;
 }
 
 export interface SendSMSAPIMessage {
-    price: number
-    total_sent: number
-    recipients: number
-    message_per_recipient: number
-    send_at: string
-    scheduled_message_id: number
-    priority_mode: boolean
-    preview: boolean
-    lead_id: number
+    price: number;
+    total_sent: number;
+    recipients: number;
+    message_per_recipient: number;
+    send_at: string;
+    scheduled_message_id: number;
+    priority_mode: boolean;
+    preview: boolean;
+    lead_id: number;
 }
 
 export interface ScheduledMessage {
-    id: number
-    send_at: string
-    recipients: number
-    content: string
-    sent: number
+    id: number;
+    send_at: string;
+    recipients: number;
+    content: string;
+    sent: number;
 }
 
-export interface ListScheduledMessageResponse extends Array<ScheduledMessage>{
-    error?: string
-    explain?: string
+export interface ListScheduledMessageResponse extends Array<ScheduledMessage> {
+    error?: string;
+    explain?: string;
 }
 
 export interface BalanceAPIResponse extends ErrorResponse {
-    balance?: string
+    balance?: string;
 }
 
 export interface BalanceResponse extends BalanceAPIResponse {}
 
 export interface DeleteMessageObject {
-    message_id: number
+    message_id: number;
 }
 
 export interface DeleteScheduledMessageResponse extends ErrorResponse {
-    success?: boolean
+    success?: boolean;
 }
 
 export interface CreateLeadOption {
-    name: string
+    name: string;
 }
 
-export interface CreateLeadAPIResponse extends ErrorResponse{
-    id?: number,
-    name?: string
+export interface CreateLeadAPIResponse extends ErrorResponse {
+    id?: number;
+    name?: string;
 }
 
 export interface CreateLeadResponse extends CreateLeadAPIResponse {}
 
 export interface GetLeadAPIResponse {
-    id: number
-    name: string
+    id: number;
+    name: string;
 }
 
 export interface GetLeadResponse extends GetLeadAPIResponse {}
 
 export interface CreateContactOption {
-    phone: string
-    lead_id: number
+    phone: string;
+    lead_id: number;
 }
 
 export interface CreateContactAPIResponse extends ErrorResponse {
-    lead_id?: number
-    contact_id?: number
-    phone?: string
+    lead_id?: number;
+    contact_id?: number;
+    phone?: string;
 }
 
 export interface CreateContactResponse extends CreateContactAPIResponse {}
 
 export interface GetContactListOption {
-    lead_id: number
+    lead_id: number;
 }
 
 export interface Contacts {
-    [key: string] : string
+    [key: string]: string;
 }
 
 export interface GetContactListAPIResponse extends ErrorResponse {
-    lead?: GetLeadResponse,
-    contacts?: Contacts
+    lead?: GetLeadResponse;
+    contacts?: Contacts;
 }
 
 export interface GetContactListResponse extends GetContactListAPIResponse {}
 
 export interface DeleteLeadOption {
-    lead_id: number
+    lead_id: number;
 }
 
 export interface DeleteLeadAPIResponse extends ErrorResponse {
-    success?: boolean
+    success?: boolean;
 }
 
 export interface DeleteLeadResponse extends DeleteLeadAPIResponse {}
 
 export interface DeleteContactOption {
-    contact_id: number
-    lead_id: number
+    contact_id: number;
+    lead_id: number;
 }
 
 export interface DeleteContactAPIResponse extends ErrorResponse {
-    success?: boolean
+    success?: boolean;
 }
 
 export interface DeleteContactResponse extends DeleteContactAPIResponse {}
