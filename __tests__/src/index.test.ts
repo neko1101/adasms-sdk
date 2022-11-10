c17c1d2c71eaca168c4343f7175c499dc4293946
Neko <38586112+neko1101@users.noreply.github.com>
2022-11-10 17:38:36 +0800

    Finalize v0.0.6 (#14)
    * feat(repo): adding npm skel files
    
    * feat(class): core function
    
    * feat(deps): update packages
    
    * Update README.md
    
    * feat(core): finished initial commit
    
    * Update tslint.json
    
    * Create npm-publish.yml
    
    * Update npm-publish.yml
    
    * Update package.json
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update npm-publish.yml
    
    * Update README.md
    
    * chore: remove test package
    
    * Update package.json
    
    * Update package.json
    
    * feat(test): added jest test and test ci
    
    * chore(deps): update package
    
    * chore(core): refactored
    
    * chore(test): fix error
    
    * Update tsconfig.json
    
    * Update tsconfig.json
    
    * Update package.json
    
    * Update tsconfig.json
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * Update index.test.ts
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * chore(workflows): updated ci
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * chore(workflows): add coverage
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * Update jest-test.yml
    
    * feat(workflow): separate coverage ci
    
    * Update index.test.ts
    
    * Create CODE_OF_CONDUCT.md
    
    * Update package.json
    
    * Update README.md
    
    * Update index.ts

--- __tests__/src/index.test.ts
+++ __tests__/src/index.test.ts
@@ -73,7 +73,7 @@ it('should send sms with send_at', async () => {
     }
     const response = await client.sendSMS(params)
     expectTypeOf(response).toMatchTypeOf<type.SendSMSResponse>()
-    expect(response.message!.send_at).toBe(params.send_at + ":00")
+    expect(response.message?.send_at).toBe(params.send_at + ":00")
 });
 
 it('should check for account balance', async () => {
@@ -90,7 +90,7 @@ it('should create lead', async () => {
     expectTypeOf(response).toMatchTypeOf<type.CreateLeadResponse>()
     expect(response.id).toBeDefined()
 
-    await cleanupLead(response.id)
+    await cleanupLead({ lead_id: response.id.toString()})
 });
 
 it('should delete lead_id', async () => {
@@ -100,7 +100,7 @@ it('should delete lead_id', async () => {
     const lead = await client.createLead(leadParams)
 
     const params: type.DeleteLeadOption = {
-        lead_id: parseInt(lead.id)
+        lead_id: lead.id
     }
     const response = await client.deleteLead(params)
     expectTypeOf(response).toMatchTypeOf<type.DeleteContactResponse>()
@@ -118,7 +118,7 @@ it('should list leads', async () => {
     // @ts-ignore
     expect(response.length).toBeGreaterThan(0)
 
-    await cleanupLead({lead_id: lead.id})
+    await cleanupLead({lead_id: lead.id.toString()})
 });
 
 it('should add contact in lead', async () => {
@@ -135,7 +135,7 @@ it('should add contact in lead', async () => {
     expectTypeOf(response).toMatchTypeOf<type.CreateContactResponse>()
     expect(response.contact_id).toBeDefined()
 
-    await cleanupLead({lead_id: lead.id})
+    await cleanupLead({lead_id: lead.id.toString()})
 });
 
 it('should get contacts for lead_id', async () => {
@@ -155,9 +155,10 @@ it('should get contacts for lead_id', async () => {
     }
     const response = await client.getContactList(params)
     expectTypeOf(response).toMatchTypeOf<type.GetContactListResponse>()
-    expect(response.contacts![contact.contact_id!]).toBeDefined()
+    // @ts-ignore
+    expect(response.contacts[contact.contact_id]).toBeDefined()
 
-    await cleanupLead({lead_id: lead.id})
+    await cleanupLead({lead_id: lead.id.toString()})
 });
 
 it('should delete contact in lead_id', async () => {
@@ -180,7 +181,7 @@ it('should delete contact in lead_id', async () => {
     expectTypeOf(response).toMatchTypeOf<type.DeleteContactResponse>()
     expect(response.success).toBeTruthy()
 
-    await cleanupLead({lead_id: lead.id})
+    await cleanupLead({lead_id: lead.id.toString()})
 });
 
 it('should able to delete scheduled message', async () => {
@@ -193,7 +194,7 @@ it('should able to delete scheduled message', async () => {
     const sms = await client.sendSMS(sendSMSParams)
 
     const params: type.DeleteMessageObject = {
-        message_id: sms.message!.scheduled_message_id
+        message_id: sms.message?.scheduled_message_id
     }
     const response = await client.deleteScheduledMessage(params)
     expectTypeOf(response).toMatchTypeOf<type.DeleteScheduledMessageResponse>()
@@ -210,7 +211,7 @@ it('should list scheduled messages', async () => {
     const sms = await client.sendSMS(sendSMSParams)
 
     const response = await client.listScheduledMessage()
-    expectTypeOf(response).toMatchTypeOf<type.ListScheduledMessageResponse>()
+    expectTypeOf(response).toMatchTypeOf<type.ListScheduledMessageResponse | type.ErrorResponse>()
     await cleanupScheduledSMS({message_id: sms.message!.scheduled_message_id})
 });
 
